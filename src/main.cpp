954f739460e68613cf89bcce2f4e909e8d664480
Charlie <200533963@student.georgianc.on.ca>
2024-10-18 19:03:44 -0400

    bunch of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ enum TileType : int
     GRASS,      // Marks unoccupied space, can be overwritten 
     DIRT,       // Marks the path, cannot be overwritten
     WAYPOINT,   // Marks where the path turns, cannot be overwritten
-    TURRET,     // 
+    TURRET,     // Marks where a turret is
     COUNT
 };
 
@@ -99,17 +99,24 @@ std::vector<Cell> FloodFill(Cell start, int tiles[TILE_COUNT][TILE_COUNT], TileT
     return result;
 }
 
-struct Enemy 
+struct Enemy // Creates reusable structure, enemies are built off of
 {
-    
+    Vector2 position{};
+    int health = 5;
+    size_t curr = 0;
+    size_t next = curr + 1;
+    bool atEnd = false;
 };
 
-struct Turret
+struct Turret // Creates reusable structure, turrets are built off of
 {
-
+    Vector2 position{};
+    float range = 200;
+    float calmDown = 0;
+    float fireRate = 0.69f;
 };
 
-struct Bullet
+struct Bullet // Creates reusable structure, bullets are built off of
 {
     Vector2 position{};
     Vector2 direction{};
@@ -125,31 +132,40 @@ int main()
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0 }, // 0
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 1
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 2
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 3
+            { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0 }, // 3
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 4
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 5
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 6
             { 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0 }, // 7
             { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 8
             { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 9
-            { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 10
+            { 0, 0, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0 }, // 10
             { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 11
             { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 12
             { 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0 }, // 13
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 14
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 15
+            { 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 3 }, // 15
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // 16
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0 }, // 17
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 18
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }  // 19
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0 }  // 19
     };
 
-    std::vector<Cell> waypoints = FloodFill({ 0, 12 }, tiles, WAYPOINT);
-    size_t curr = 0;
-    size_t next = curr + 1;
-    bool atEnd = false;
+    std::vector<Cell> waypoints = FloodFill({ 0, 12 }, tiles, WAYPOINT);// Created array
+    std::vector<Enemy> bullies;                                         // Created array
+    std::vector<Turret> nerds;                                          // Created array
+    for (int row = 0; row < TILE_COUNT; ++row) // Looks at every row
+    {
+        for (int col = 0; col < TILE_COUNT; ++col) // Looks at every column within that row
+        if (tiles[row][col] == 3) // Checks to see if tile is equal to 3, if it is, create turret
+        {
+            Turret turret;
+            turret.position = TileCenter(row, col);
+            nerds.push_back(turret);
+        }
+    }
 
-    Vector2 enemyPosition = TileCenter(waypoints[curr].row, waypoints[curr].col);
+
     const float enemySpeed = 250.0f;
     const float enemyRadius = 20.0f;
 
@@ -159,44 +175,76 @@ int main()
 
     std::vector<Bullet> bullets;
     float shootCurrent = 0.0f;
-    float shootTotal = 0.25f;
+    float shootTotal = 0.42f;
+
+    float enemyTime = 0.00f;
+    int totalEnemies = 10; // Maximum amount of spawnable enemies
+    int enemyCount = 0; // Amount of enemies that have been spawned
 
     InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Tower Defense");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-        Vector2 mouse = GetMousePosition();
-        Cell mouseCell;
-        mouseCell.col = mouse.x / TILE_SIZE;
-        mouseCell.row = mouse.y / TILE_SIZE;
+        Vector2 mouse = GetMousePosition();  // For Assignment 1
+        Cell mouseCell;                      // For Assignment 1
+        mouseCell.col = mouse.x / TILE_SIZE; // For Assignment 1
+        mouseCell.row = mouse.y / TILE_SIZE; // For Assignment 1
+        enemyTime += dt; // Enemy spawn timer, going up by DeltaTime
+
+        if (enemyTime >= 1 && enemyCount < totalEnemies) // If time since enemy spawned is greater than or equal to 1, spawn a new enemy
+        {
+            Enemy enemy;
+            enemy.position = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
+            enemy.curr = 0;
+            enemy.next = enemy.curr + 1;
+            enemyTime = 0;
+            enemy.atEnd = false;
+            bullies.push_back(enemy); // Pushes to the bullies vector
+            enemyCount++;
+        }
 
         // Path following
-        if (!atEnd)
+        for (Enemy& enemy:bullies)
         {
-            Vector2 from = TileCenter(waypoints[curr].row, waypoints[curr].col);
-            Vector2 to = TileCenter(waypoints[next].row, waypoints[next].col);
-            Vector2 direction = Normalize(to - from);
-            enemyPosition = enemyPosition + direction * enemySpeed * dt;
-            if (CheckCollisionPointCircle(enemyPosition, to, enemyRadius))
+            if (!enemy.atEnd) // If the enemy is not at the end, continue through path following
             {
-                curr++;
-                next++;
-                atEnd = next == waypoints.size();
-                enemyPosition = TileCenter(waypoints[curr].row, waypoints[curr].col);
+                Vector2 from = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
+                Vector2 to = TileCenter(waypoints[enemy.next].row, waypoints[enemy.next].col);
+                Vector2 direction = Normalize(to - from);
+                enemy.position = enemy.position + direction * enemySpeed * dt;
+                if (CheckCollisionPointCircle(enemy.position, to, enemyRadius))
+                {
+                    enemy.curr++;
+                    enemy.next++;
+                    enemy.atEnd = enemy.next == waypoints.size();
+                    enemy.position = TileCenter(waypoints[enemy.curr].row, waypoints[enemy.curr].col);
+                }
             }
         }
 
-        // Shooting
-        shootCurrent += dt;
-        if (shootCurrent >= shootTotal)
+        for (Turret& turret:nerds) 
         {
-            shootCurrent = 0.0f;
+            turret.calmDown += dt;
+            Enemy* meanies = nullptr;
+            
+            for (Enemy& enemy:bullies) // For every enemy, check to see if the distance is within the turrets range, then declaring them a meanie if they are
+            {
+                float enemyDistance = Distance(turret.position, enemy.position);
+                if (enemyDistance < turret.range)
+                {
+                    meanies = &enemy;
+                }
+            }
+            if (meanies && turret.calmDown >= turret.fireRate) // If turret isn't on cooldown, fire at the meanies and reset cooldown
+            {
+                turret.calmDown = 0.0f;
 
-            Bullet bullet;
-            bullet.position = mouse;
-            bullet.direction = Normalize(enemyPosition - bullet.position);
-            bullets.push_back(bullet);
+                Bullet bullet;
+                bullet.position = turret.position;
+                bullet.direction = Normalize(meanies->position - bullet.position); // Accesses position of a specific target, the meanies
+                bullets.push_back(bullet); // Pushes to the bullet vector
+            }
         }
 
         // Bullet update
@@ -206,10 +254,27 @@ int main()
             bullet.time += dt;
 
             bool expired = bullet.time >= bulletTime;
-            bool collision = CheckCollisionCircles(enemyPosition, enemyRadius, bullet.position, bulletRadius);
-            bullet.enabled = !expired && !collision;
+            for (int i = 0; i < bullies.size();)
+            {
+                Enemy& enemy = bullies[i]; // Referencing a specific enemy within enemies vector
+                bool collision = CheckCollisionCircles(enemy.position, enemyRadius, bullet.position, bulletRadius);
+                if (collision)
+                {
+                    enemy.health--; // Takes a health away from bully for every time they get hit
+                    if (enemy.health <= 0) // Erases the bully that has no more health
+                    {
+                        bullies.erase(bullies.begin() + i);
+                        bullet.enabled = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            bullet.enabled = !expired && bullet.enabled; // Makes it so it is enabled as long as it is not expired and enabled
         }
-        //Enemy removal
         
         // Bullet removal
         bullets.erase(std::remove_if(bullets.begin(), bullets.end(),
@@ -226,12 +291,19 @@ int main()
                 DrawTile(row, col, tiles[row][col]);
             }
         }
-        DrawCircleV(enemyPosition, enemyRadius, RED);
 
         // Render bullets
         for (const Bullet& bullet : bullets)
             DrawCircleV(bullet.position, bulletRadius, BLUE);
+        // Render turrets
+        for (const Turret& turret : nerds)
+            DrawCircleV(turret.position, bulletRadius, DARKGRAY);
+        // Render enemies
+        for (const Enemy& enemy : bullies)
+            DrawCircleV(enemy.position, bulletRadius, RED);
+        // Printing text to make sure bullets are firing and deleting, and that only ten enemies are spawning
         DrawText(TextFormat("Total bullets: %i", bullets.size()), 10, 10, 20, BLUE);
+        DrawText(TextFormat("Total enemies: %i", enemyCount), 10, 30, 20, RED);
         DrawTile(mouseCell.row, mouseCell.col, PURPLE);
 
         EndDrawing();
